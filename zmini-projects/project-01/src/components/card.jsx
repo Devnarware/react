f1e16b33eb41be8dda3b9493818c786d3ea0a36f
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-18 20:38:42 +0530

    feat: refactor Card component to use props for dynamic content and improve naming conventions

--- zmini-projects/project-01/src/components/card.jsx
+++ zmini-projects/project-01/src/components/card.jsx
@@ -1,30 +1,30 @@
 import { Bookmark } from '../../../node_modules/lucide-react';
 
 
-const card = () => {
+const Card = (props) => {
   return (
     <div className="card">
         <div>
             <div className="top">
                 <div className="image">
-                    <img src="https://imgs.search.brave.com/-kYf5CYKNI9LVjMyP3ue1jngBaYrdPgP00RRu3ty89M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzNiLzcw/L2FjLzNiNzBhYzdj/M2RhMjhlNDdmZGVl/NGNiMjlkYmM1YzY3/LmpwZw" alt="logo" />
+                    <img src={props.brandlogo} alt="logo" />
                 </div>
                 <button>Save <Bookmark /></button>
             </div>
             <div className="center">
-                <h3>Amazon <span>5 days ago</span></h3>
-                <h2>Senior UI/UX Designer</h2>
+                <h3>{props.comapnyName} <span>{props.datePosted}</span></h3>
+                <h2>{props.post}</h2>
                 <div className="tags">
-                    <h4>Part-Time</h4>
-                    <h4>Senior Level</h4>
+                    <h4>{props.tag1}</h4>
+                    <h4>{props.tag2}</h4>
                 </div>
             </div>
         </div>
 
         <div className="bottom">
             <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai, India</p>
+                <h3>{props.pay}</h3>
+                <p>{props.location}</p>
             </div>
             <button>Apply Now</button>
         </div>
@@ -32,4 +32,4 @@ const card = () => {
   )
 }
 
-export default card
+export default Card ;
