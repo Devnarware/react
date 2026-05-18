88007c66f36951ce8e305e16a8aa55bf44732d0e
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-18 15:16:26 +0530

    feat: update Card component structure and styling

--- zmini-projects/project-01/src/App.jsx
+++ zmini-projects/project-01/src/App.jsx
@@ -1,9 +1,9 @@
-
+import Card from "./components/Card.jsx"
 
 const App = () => {
   return (
     <>
-        
+        <Card />
     </>
   )
 }
--- zmini-projects/project-01/src/components/card.jsx
+++ zmini-projects/project-01/src/components/card.jsx
@@ -1,11 +1,31 @@
+// import { Bookmark } from 'lucide-react';
 
 
 const card = () => {
   return (
     <div className="card">
-        <div className="top"></div>
-        <div className="midlle"></div>
-        <div className="bottom"></div>
+        <div>
+            <div className="top">
+                <img src="" alt="logo" />
+                <button>Save </button>
+            </div>
+            <div className="center">
+                <h3>Amazon <span>5 days ago</span></h3>
+                <h2>Senior UI/UX Designer</h2>
+                <div className="tags">
+                    <h4>Part-Time</h4>
+                    <h4>Senior Level</h4>
+                </div>
+            </div>
+        </div>
+        <br />
+        <div className="bottom">
+            <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai, India</p>
+            </div>
+            <button>Apply Now</button>
+        </div>
     </div>
   )
 }
