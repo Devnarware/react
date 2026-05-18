998367b30ae42abc299e6f89a20045be8f394727
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-18 07:25:22 +0530

    feat: initialize React project with Vite, add Card component and styling
    - Created package.json with dependencies and scripts for development
    - Implemented main App component to render multiple Card components
    - Developed Card component with image, title, description, and button
    - Added global CSS styles for layout and component design
    - Configured Vite for React project

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/Card.jsx"
+
+const App = () => {
+  return (
+    <div className="parent">
+      <Card></Card>
+      <Card></Card>
+    </div>
+  )
+}
+
+export default App
