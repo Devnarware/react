e4222e8e10ea212f073f4a41bd7f29e957c9ffd5
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-18 07:42:49 +0530

    feat: update Card component to accept props for image and name

--- 03-props/src/components/Card.jsx
+++ 03-props/src/components/Card.jsx
@@ -1,12 +1,14 @@
 
-const img = "https://images.unsplash.com/photo-1772371272179-3ecc656fc677?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
+const Card = (props) => {
 
-const Card = () => {
+  console.log(props);
+  
+
   return (
     <div className="card">
-        <img src={img} alt="pfp" />
-        <h1>Dev</h1>
+        <img src={props.img} alt="pfp" />
+        <h1>{props.name}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, placeat!</p>
         <button>Click me</button>
 
