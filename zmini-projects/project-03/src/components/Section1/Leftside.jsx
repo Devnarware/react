557ee3a21f51fac204f854044b9b0251c447e309
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-21 16:45:56 +0530

    feat: update padding for customer segmentation label in Leftside component

--- zmini-projects/project-03/src/components/Section1/Leftside.jsx
+++ zmini-projects/project-03/src/components/Section1/Leftside.jsx
@@ -4,7 +4,7 @@ function Leftside() {
   return (
     <div className="h-full w-1/3  flex flex-col justify-between py-10">
       <div className="text flex flex-col ">
-        <h2 className='text-6xl/19 font-bold font-font-sans font-stretch-110% tracking-tight mb-15'>Prospective <span className='bg-gray-100 p-1 rounded-full'>customer</span> segmentation</h2>
+        <h2 className='text-6xl/19 font-bold font-font-sans font-stretch-110% tracking-tight mb-15'>Prospective <span className='bg-gray-100 px-3 py-2 rounded-full'>customer</span> segmentation</h2>
         <p className='text-2xl/10 font-sans font-stretch-110% font-extralight'> Depending on customer satisfaction and access to banking products, potential target audience can be divided into three groups</p>
       </div>
       <div className="flex items-baseline-last">
