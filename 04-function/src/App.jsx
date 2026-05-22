c2f49b2a123ba3a56f1c4f6bcd4c2c630d7387d5
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-22 16:46:07 +0530

    feat: integrate Tailwind CSS and update dependencies
    - Added Tailwind CSS and its Vite plugin to the project.
    - Updated package.json and package-lock.json to include "@tailwindcss/vite" and "tailwindcss" dependencies.
    - Removed the Card component and its usage in App.jsx.
    - Simplified App.jsx to an empty functional component.
    - Cleaned up index.css to only import Tailwind CSS.
    - Updated vite.config.js to include the Tailwind CSS plugin.

--- 04-function/src/App.jsx
+++ 04-function/src/App.jsx
@@ -1,14 +1,10 @@
-import Card from "./components/Card.jsx"
 
-const img = "https://images.unsplash.com/photo-1772371272179-3ecc656fc677?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-const img2 = "https://images.unsplash.com/photo-1740252117044-2af197eea287?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
-const App = () => {
+function App() {
   return (
-    <div className="parent">
-      <Card img = {img} name = "Dev"></Card>
-      <Card img = {img2} name = "Devansh"></Card>
-    </div>
+    <>
+      
+    </>
   )
 }
 
