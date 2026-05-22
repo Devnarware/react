6ee98e90d418136e3b27465f2e4112350b18a7e8
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-22 16:39:49 +0530

    initial commit

--- zmini-projects/project-03/src/components/Section1/Rightside.jsx
+++ zmini-projects/project-03/src/components/Section1/Rightside.jsx
@@ -11,6 +11,22 @@ function Rightside() {
 
         <CardContent />
       </div>
+      <div className=' h-[90%] w-95 rounded-[50px] overflow-hidden relative'>
+        <img src="https://images.unsplash.com/photo-1615840287409-bda7eedb0d09?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="img" 
+        
+          className='object-cover h-full '
+        />
+
+        <CardContent />
+      </div>
+      <div className=' h-[90%] w-95 rounded-[50px] overflow-hidden relative'>
+        <img src="https://images.unsplash.com/photo-1602803056945-ebac8ae8fd00?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="img" 
+        
+          className='object-cover h-full '
+        />
+
+        <CardContent />
+      </div>
      
       
       
