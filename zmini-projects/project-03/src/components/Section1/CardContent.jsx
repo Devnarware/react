7d04676b6b63335fb33c44d5721356788f1b1093
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-22 13:59:13 +0530

    feat: enhance CardContent and Rightside components with improved layout and styling

--- zmini-projects/project-03/src/components/Section1/CardContent.jsx
+++ zmini-projects/project-03/src/components/Section1/CardContent.jsx
@@ -1,15 +1,15 @@
-
+import { ChevronRight } from 'lucide-react';
 
 function CardContent() {
   return (
-    <div className=" h-[90%] w-95 rounded-[50px] overflow-hidden bg-transparent z-10">
-        <h2>1</h2>
-        <div>
-            <p> prime customers that have access to bank credit and are satisfied with the current product</p>
-            <div>
-                <button>Satisfied</button>
-                <button>
-                    {/* <ChevronRight /> */}
+    <div className="absolute top-0 h-full w-95 rounded-[50px] p-10 flex flex-col justify-between bg-gray-500">
+        <h2 className="h-15 w-15 bg-gray-100 rounded-full flex items-center justify-center text-3xl">1</h2>
+        <div className="  text-2xl/10 font-sans font-stretch-120% font-light text-gray-100">
+            <p className="mb-15"> Prime customers, that have access to bank credit and are satisfied with the current product</p>
+            <div className='flex justify-between items-center' >
+                <button className='bg-blue-500 px-6 py-2 rounded-full text-white'>Satisfied</button>
+                <button className='bg-blue-500 p-2 flex justify-center items-center rounded-full'>
+                    <ChevronRight size={35} />
                 </button>
             </div>
         </div>
--- zmini-projects/project-03/src/components/Section1/Rightside.jsx
+++ zmini-projects/project-03/src/components/Section1/Rightside.jsx
@@ -4,7 +4,10 @@ function Rightside() {
   return (
     <div className='h-full w-full bg-gray-700 flex gap-10'>
       <div className=' h-[90%] w-95 rounded-[50px] overflow-hidden relative'>
-        <img src="https://images.unsplash.com/photo-1696453424412-fe6ea4b2db54?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className='object-cover h-full '/>
+        <img src="https://images.unsplash.com/photo-1696453424412-fe6ea4b2db54?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="img" 
+        
+          className='object-cover h-full '
+        />
 
         <CardContent />
       </div>
