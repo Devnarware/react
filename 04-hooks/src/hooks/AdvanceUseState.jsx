c026e74e48b3e72a613c77298f56d4eabd993615
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-25 20:29:21 +0530

    feat: update AdvanceUseState to modify object state and enhance button styling

--- 04-hooks/src/hooks/AdvanceUseState.jsx
+++ 04-hooks/src/hooks/AdvanceUseState.jsx
@@ -4,7 +4,10 @@ import { useState } from "react";
 function AdvanceUseState() {
 
     const btnClicked = () =>{
-        console.log("hello");
+        const obj2 = {...obj}
+        obj2.name = "david"
+        setName(obj2)
+        console.log(obj, obj2);    
     }
 
     const [obj, setName] = useState({name: "Dev", age: 23})
@@ -12,7 +15,7 @@ function AdvanceUseState() {
   return (
     <div>
         <h1 className="text-3xl ">{obj.name}, { obj.age}</h1>
-        <button onClick={btnClicked} className="text-3xl p-10 border-2">Click me</button>
+        <button onClick={btnClicked} className="text-3xl p-10 border-2 cursor-pointer">Click me</button>
     </div>
   )
 }
