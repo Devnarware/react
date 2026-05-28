fe893346c6c964b83038e38d28e63f36f9be7a2f
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-28 12:22:52 +0530

    feat: update Input component with improved form layout and styling

--- react-project/01-notes-app/src/components/Input.jsx
+++ react-project/01-notes-app/src/components/Input.jsx
@@ -4,16 +4,18 @@
 const Input = () => {
   return (
     <div>
-        <form className='flex flex-col text-4xl w-1/3'>
+        <form className='flex flex-col text-4xl w-1/3 gap-3'>
             <input type="text" 
-                placeholder='Enter the title'
-                className='border-3 px-5 py-2'
+                placeholder='Enter the title here'
+                className='border-3 px-5 py-2 rounded'
             />
             <textarea 
-                className='border-3 px-5 py-2 '
-                placeholder='Enter the description'>
+                className='border-3 px-5 py-2 h-50 rounded'
+                placeholder='Write the details here'>
             </textarea>
-            <button>Submit</button>
+            <button 
+                className="px-5 py-2 bg-[#c5c5c5] text-[#000000] rounded-2xl"
+            >Add Note</button>
         </form>
     </div>
   )
