a038ea6b12b0bae8967cc695c79ca07d914ef17a
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-28 16:31:38 +0530

    Taking input through react instead of DOM using "two way binding"

--- react-project/01-notes-app/src/components/Input.jsx
+++ react-project/01-notes-app/src/components/Input.jsx
@@ -1,20 +1,34 @@
+import { useState } from "react"
 
 
 
-const Input = () => {
+const Input = (props) => {
+
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
     return (
-        <div className="lg:w-[40%] h-screen  border-r-4">
+        <div className="lg:w-[40%] lg:h-screen  border-r-4">
             <div className="m-12">
                 <h2 className="text-5xl font-bold mt-15 mb-10">Add Note</h2>
                 <form
-                    className='flex flex-col gap-3 '>
+                    className='flex flex-col gap-4 '>
                     <input type="text"
                         placeholder='Enter the title here'
                         className='border-3 px-6 py-4 rounded text-2xl'
+                        value={title}
+                        onChange={(e)=>{
+                            setTitle(e.target.value)
+                        }}
                     />
                     <textarea
                         className='border-3 px-6 py-3 h-50 rounded text-2xl'
-                        placeholder='Write the details here'>
+                        placeholder='Write the details here'
+                        value={details}
+                        onChange={(e)=>{
+                            setDetails(e.target.value)
+                        }}
+                    >
                     </textarea>
                     <button
                         className="px-5 py-2 bg-[#c5c5c5] text-[#000000] rounded-2xl text-3xl"
--- react-project/01-notes-app/src/components/NotesApp.jsx
+++ react-project/01-notes-app/src/components/NotesApp.jsx
@@ -1,10 +1,16 @@
 import Input from './Input.jsx'
 import Display from './Display.jsx'
+import { useState } from 'react'
+
+
 
 const NotesApp = () => {
+
+  const [task, setTask] = useState([])
+
   return (
-    <div className='flex '>
-        <Input />
+    <div className='lg:flex '>
+        <Input arr={task}/>
         <Display />
     </div>        
   )
