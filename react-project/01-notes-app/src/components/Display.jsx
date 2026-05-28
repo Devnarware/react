b0cb6e7b344b5b0920b32b550436991f0d673c19
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-28 17:16:58 +0530

    Complete the notes display functionality

--- react-project/01-notes-app/src/components/Display.jsx
+++ react-project/01-notes-app/src/components/Display.jsx
@@ -7,25 +7,23 @@ const Display = (props) => {
         <h2 className="text-5xl font-bold mt-15 mb-10">Recent Notes</h2>
         <div className='flex flex-wrap gap-9 h-245 overflow-y-auto'>
 
-          
-          <div className="h-104 w-75 rounded-2xl bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] bg-cover overflow-hidden px-10 pt-11 flex flex-col justify-between"
-          >
-            
-            {props.task.map(function(elem, idx){
-              return(
+
+          {props.task.map(function (elem, idx) {
+            return (
+              <div className="h-104 w-75 rounded-2xl bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] bg-cover overflow-hidden px-10 pt-11 flex flex-col justify-between"
+              >
+
                 <div key={idx} className="flex flex-col gap-3 h-[90%]  ">
-                  <h3 className="text-3xl font-bold text-[#111] leading-8 "></h3>
-                  <p className=" text-[#5a5a5a] h-[90%] overflow-auto"></p>
+                  <h3 className="text-3xl font-bold text-[#111] leading-8 ">{elem.title}</h3>
+                  <p className=" text-[#5a5a5a] h-[90%] overflow-auto">{elem.details}</p>
+                </div>
+                
+                <button className=" h-[12%] w-[90%] bg-red-500 mb-2 rounded-2xl text-[#fcfcfc]">Delete</button>
               </div>
-              )
-            })}
-            
+            )
+          })}
 
 
-            <button className=" h-[12%] w-[90%] bg-red-500 mb-2 rounded-2xl text-[#fcfcfc]">Delete</button>
-          </div>
-          
-          
         </div>
       </div>
     </div>
