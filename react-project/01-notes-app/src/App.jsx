2cb4af0b8d79074cc7a983c191f750510d70bd65
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-28 11:28:46 +0530

    feat: implement NotesApp component with Input and Display components

---
+++ react-project/01-notes-app/src/App.jsx
@@ -0,0 +1,11 @@
+import NotesApp from './components/NotesApp.jsx'
+
+const App = () => {
+  return (
+    <div>
+        <NotesApp />
+    </div>
+  )
+}
+
+export default App
---
+++ react-project/01-notes-app/src/components/NotesApp.jsx
@@ -0,0 +1,13 @@
+import Input from './Input.jsx'
+import Display from './Display.jsx'
+
+const NotesApp = () => {
+  return (
+    <div>
+        <Input />
+        <Display />
+    </div>        
+  )
+}
+
+export default NotesApp
