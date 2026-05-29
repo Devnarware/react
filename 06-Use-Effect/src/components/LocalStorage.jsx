6321ab6b058e971cec984cda86a360a5bd333385
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-29 18:14:30 +0530

    Understanding the behavior of localstorage

--- 06-Use-Effect/src/components/LocalStorage.jsx
+++ 06-Use-Effect/src/components/LocalStorage.jsx
@@ -1,13 +1,29 @@
 
 
 const LocalStorage = () => {
-
-   localStorage.clear()
+   
+    // const user = {
+    //     name: "Dev",
+    //     age: 20,
+    //     mail: "dev@example.com"
+    // }    
     
-     
+    // user.gender = 'male'
+    
+
+    // const userInfo = JSON.stringify(user)
+    // localStorage.setItem('userInfo', userInfo)
+
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"))
+
+    
+
   return (
     <div className="m-10 text-3xl">
-        
+        <h2>{userInfo.name}</h2>
+        <h2>{userInfo.mail}</h2>
+        <h2>{userInfo.age}</h2>
+        <h2>{userInfo.gender}</h2>
     </div>
   )
 }
