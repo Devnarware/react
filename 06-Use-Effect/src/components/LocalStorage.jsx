5b46fb183fa32053cfe99392262749bd17fbff6c
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-29 18:32:22 +0530

    understand the localstorage completely

--- 06-Use-Effect/src/components/LocalStorage.jsx
+++ 06-Use-Effect/src/components/LocalStorage.jsx
@@ -14,14 +14,32 @@ const LocalStorage = () => {
 
     const allUsers = JSON.stringify(users)
     localStorage.setItem("All users info", allUsers)
+    
 
+    const data = JSON.parse(localStorage.getItem("All users info"))
+
+        data.map((elem, idx) => {
+            console.log(elem.name, idx);
+            
+        })
+    
     
 
     
 
   return (
     <div className="m-10 text-3xl">
-        
+        {data.map((elem, idx)=>{
+            return (
+                <div>
+                    <h1>for the {idx +1}th user </h1>
+                    <h2>{elem.name}</h2>
+                    <h2>{elem.age}</h2>
+                    <h2>{elem.mail}</h2>
+                    <hr />
+                </div>
+            )
+        })}
     </div>
   )
 }
