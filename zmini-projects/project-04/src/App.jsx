2d218c233845bdd7e77263accee27feb0d2947cb
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-01 20:10:26 +0530

    Install the css

---
+++ zmini-projects/project-04/src/App.jsx
@@ -0,0 +1,11 @@
+import Gallery from "./components/Gallery.jsx"
+
+const App = () => {
+  return (
+    <div>
+      <Gallery />
+    </div>
+  )
+}
+
+export default App
---
+++ zmini-projects/project-04/src/components/Gallery.jsx
@@ -0,0 +1,11 @@
+
+
+const Gallery = () => {
+  return (
+    <div className="h-screen w-full text-3xl bg-[#0c1216]">
+        Gallery
+    </div>
+  )
+}
+
+export default Gallery
