59af98ab65b61d92758cfd11fafb999831f91a1f
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-01 22:52:43 +0530

    Add axios for API calls and update Gallery component to fetch images

--- zmini-projects/project-04/src/components/Gallery.jsx
+++ zmini-projects/project-04/src/components/Gallery.jsx
@@ -1,10 +1,44 @@
-
+import axios from 'axios'
+import { useState } from 'react'
 
 const Gallery = () => {
+
+  const [data, setData] = useState([])
+
+  const apiCall = async () => {
+
+    const response = await axios('https://picsum.photos/v2/list?page=2&limit=10')
+    // const data = await response.data
+    setData(response.data)
+
+  }
+
+  let showImg = 'EMPTY'
+  if (data.length > 0) {
+    setImg = data.map((elem, idx) => {
+      return (
+        // <img src={elem.data} alt={idx} />
+        "HELLO"
+      )
+    })
+  }
+
+
   return (
+
     <div className="h-screen w-full text-3xl bg-[#0c1216]">
-        Gallery
+
+      <button
+        className="text-3xl text-[#3d3d3d] m-10 py-4 px-6 rounded-3xl bg-[#4fd4d4]"
+        onClick={apiCall}
+      >Click me</button>
+
+      <div>
+        {showImg}
+      </div>
+
     </div>
+
   )
 }
 
