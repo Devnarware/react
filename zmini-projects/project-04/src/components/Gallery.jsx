64e0bbf2fde76f62e13a3a467f4d615e4ef2eb1d
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-02 15:28:55 +0530

    Update Gallery component to enhance data fetching and rendering logic

--- zmini-projects/project-04/src/components/Gallery.jsx
+++ zmini-projects/project-04/src/components/Gallery.jsx
@@ -1,35 +1,46 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Gallery = () => {
 
   const [userData, setUserData] = useState([])
 
-  const getData = async()=>{
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=5')
-    setUserData(response.data)
+
+  const getData = async () => {
+    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=500')
   }
 
+  
+
   let showData = "Nothing to show here"
-  if (userData.length>1) {
-    showData = userData.map(function(elem, idx){
-      return(
-        <div key={idx}>
-          <a href={elem.url}><img src={elem.download_url} alt={idx} /></a>
+  if (userData.length > 1) {
+    showData = userData.map(function (elem, idx) {
+      return (
+        <div key={idx} className='flex flex-col gap-5'>
+          <a href={elem.url} target='_blank'>
+            <div>
+              <img src={elem.download_url} alt={idx}
+                className='h-60 object-cover'
+              />
+            </div>
+          </a>
+          <h1 className='text-2xl text-center'>{elem.author}</h1>
         </div>
       )
     })
   }
 
   return (
-    <div className="h-screen w-full bg-[#131723]">
-      <button 
+    <div className="h-full w-full bg-[#131723]">
+      {/* <button
         className="m-10 py-3 px-6  bg-[#70e0e6] rounded-2xl text-3xl text-[#303030]"
         onClick={getData}
       >
         Click Me
-      </button>
-      <div>
+      </button> */}
+
+      
+      <div className='flex gap-10 flex-wrap mx-25 my-10 justify-around'>
         {showData}
       </div>
     </div>
