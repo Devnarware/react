3e53fcc9d90e863ebc0d4ca2a8b0ce60e081bbe6
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-04 20:10:26 +0530

    feat: implement routing with Navbar component and wrap App in BrowserRouter

--- 07-React-router-DOM/src/App.jsx
+++ 07-React-router-DOM/src/App.jsx
@@ -1,9 +1,12 @@
-
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './pages/Navbar.jsx'
 
 const App = () => {
   return (
     <div className="h-screen bg-gray-700">
-      
+      <Routes>
+        <Navbar />
+      </Routes>
     </div>
   )
 }
