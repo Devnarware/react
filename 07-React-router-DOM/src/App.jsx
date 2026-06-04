90e648219e9fede60390c55a1e6b02a8a1eef137
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-04 21:13:05 +0530

    feat: add About and Product routes to App component

--- 07-React-router-DOM/src/App.jsx
+++ 07-React-router-DOM/src/App.jsx
@@ -1,5 +1,9 @@
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Product from './pages/Product.jsx'
+
 import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
 
@@ -12,6 +16,8 @@ const App = () => {
 
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/product' element={<Product />} />
       </Routes>
       
     </div>
